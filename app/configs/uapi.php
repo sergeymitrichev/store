244e7141efbb338618d80e94a556b722482021e5
Sergey Mitrichev <sergeymitrichev@yandex.ru>
2018-01-23 17:43:41 +0300

    Update uapi.php

--- app/configs/uapi.php
+++ app/configs/uapi.php
@@ -1,10 +1,10 @@
 <?php
 
 return array(
-    'site'              =>  'https://justamin.ucoz.ru/',
-    'consumer_key'      =>  'm_cat',
-    'consumer_secret'   => '1l32WfoVwcJMIpbYKYfiP31M4dvKOA',
-    'token'             => 'vzlTMxDZuqBIJRmspXswJmWUnNLaexqlW32KMJSO',
-    'token_secret'      => 'Ucrwq.ptKgmsnW9yY6cxnzDHyCe7AyG3xpu3f6bf',
+    'site'              =>  '',
+    'consumer_key'      =>  '',
+    'consumer_secret'   => '',
+    'token'             => '',
+    'token_secret'      => '',
     'version'           => \APIuCoz\ApiClient::V1
 );
