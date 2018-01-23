2c971261ad1cd402ceac03bc9ee78744c11d624c
Sergey Mitrichev <sergeymitrichev@yandex.ru>
2018-01-23 17:43:01 +0300

    Update retailcrm.php

--- app/configs/retailcrm.php
+++ app/configs/retailcrm.php
@@ -1,7 +1,7 @@
 <?php
 
 return array(
-    'url'              =>  'https://minutka-nn.retailcrm.ru/',
-    'api_key'      =>      'KVDaydJnXadyR2v5Z3zGlGDdOMgb4vwR',
+    'url'              =>  '',
+    'api_key'      =>      '',
     'version'           => \RetailCrm\ApiClient::V5
 );
